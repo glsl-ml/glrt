b36a5d914ddb26e58aa0e70072956045ab7c8403
aditya4d <2188479+aditya4d@users.noreply.github.com>
2026-04-24 14:47:18 -0700

    Update gpu_link_check.sh

--- deps/gpu_link_check.sh
+++ deps/gpu_link_check.sh
@@ -7,22 +7,26 @@ if [ "$EUID" -ne 0 ]; then
 fi
 
 # Header
-printf "%-12s \t %-12s \t %-s\n" "PCI ID" "WIDTH" "DEVICE NAME"
+printf "%-12s \t %-8s \t %-10s \t %-s\n" "PCI ID" "WIDTH" "SPEED" "DEVICE NAME"
 echo "------------------------------------------------------------------------------------------------"
 
 # Find all VGA and 3D controllers
 lspci -nn | grep -E "VGA|3D" | while read -r line; do
-    # Extract the PCI ID (e.g., 01:00.0)
+    # Extract the PCI ID
     pci_id=$(echo "$line" | cut -d' ' -f1)
     
-    # Clean the name: 
-    # 1. Remove the bracketed class info at the start
-    # 2. Remove the hexadecimal IDs at the end [1002:675d]
+    # Clean the name: Remove class info [0300] and hex IDs [1002:675d]
     full_name=$(echo "$line" | sed -e 's/^.*]: //' -e 's/ \[....:....\]//')
 
-    # Query lspci for the Link Status (LnkSta)
-    link_width=$(lspci -vv -s "$pci_id" 2>/dev/null | grep "LnkSta:" | grep -o "Width x[0-9]*" | cut -d' ' -f2)
+    # Query lspci for Link Status (LnkSta)
+    lnk_sta=$(lspci -vv -s "$pci_id" 2>/dev/null | grep "LnkSta:")
+
+    # Extract Width (e.g., x16)
+    link_width=$(echo "$lnk_sta" | grep -o "Width x[0-9]*" | cut -d' ' -f2)
+
+    # Extract Speed (e.g., 5GT/s or 8GT/s)
+    link_speed=$(echo "$lnk_sta" | grep -o "[0-9.]*GT/s")
 
     # Print results
-    printf "%-12s \t %-12s \t %-s\n" "$pci_id" "$link_width" "$full_name"
+    printf "%-12s \t %-8s \t %-10s \t %-s\n" "$pci_id" "$link_width" "$link_speed" "$full_name"
 done
